c3164c89b57fe0acac190c1145f3ff53ec01b491
aleksandre-svg <datodzukaevi38@gmail.com>
2024-07-26 19:13:50 +0400

    commit

---
+++ level7/homework.py
@@ -0,0 +1,17 @@
+inte = int(input("write some numbers:"))
+text = input("write some text:")
+type_one = type(inte)
+type_two = type(text)
+print(type_one)
+print(type_two)
+
+goa = "goa is best"
+type_of_string = type(goa)
+print(type_of_string)
+
+text_one = "hey"
+print(int(text_one))
+# ეს კოდი ქმნის 2 ცვლადს რომლის მნიშვნოლობებია ორი string 10 პრინტ ფუნქციაში ამ ორ ცვლადს გადააქცევს ინტეჯერად და მიუმატებს ერთმანებს რისი პასუხიცაა 20, თუ ინტეჯერად არ გადავაქცევდით იგი დაწერდა 1010
+num1 = "10"
+num2 = "10"
+print(int(num1) + int(num2))
