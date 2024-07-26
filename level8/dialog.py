4dc2b05624bcfd8bb85dfeb5f815bc1c6de5d2aa
aleksandre-svg <datodzukaevi38@gmail.com>
2024-07-26 19:14:52 +0400

    commit

---
+++ level8/dialog.py
@@ -0,0 +1,8 @@
+chatbot = input("hello!:")
+if chatbot == "hello":
+    chatbot_two = input("how are you? how can i help you?:")
+    if chatbot_two == "what weather outside?":
+        print("its hot and cloudy")
+    elif chatbot_two == "i need to find a funny and fantazy movie":
+        print("Lets watch Shrek")
+    elif chatbot_two == "i need to find magic and fantazy movie":
